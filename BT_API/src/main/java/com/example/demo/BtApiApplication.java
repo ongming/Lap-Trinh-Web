a2c2b7a8a487c1b1192af0817dcb5a1f2558daf8
Mophak <160752280+ongming@users.noreply.github.com>
2025-09-26 09:26:57 +0700

    Add files via upload

---
+++ BT_API/src/main/java/com/example/demo/BtApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtApiApplication.class, args);
+	}
+
+}
