fec6c7ee2181c3e381f11373a8afe0341fe46906
Mophak <160752280+ongming@users.noreply.github.com>
2025-10-03 08:33:01 +0700

    Add files via upload

---
+++ BT-11/src/main/java/com/example/demo/Bt11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt11Application.class, args);
+	}
+
+}
